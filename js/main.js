4b6d9e3358cff0edf2c70b2162c711138b40bd7e
giorgi <127527335+nozadze22@users.noreply.github.com>
2024-06-17 23:24:11 +0400

    done

--- js/main.js
+++ js/main.js
@@ -5,12 +5,15 @@ const ScoreDisplay = document.querySelector("#ScoreDisplay");
 const generate = document.querySelector(".generate");
 const rangePicker = document.querySelector("#rangePicker");
 const displayWrapper = document.querySelector(".wrapper");
+const arrow = document.querySelector("#arrow");
+
+
 
 
 const gameConfig = {
-    isStarted: true,
+    isStarted: false,
     score: 0,
-    amount: 10,
+    amount: rangePicker.value,
     selectedCards: []
 };
 
@@ -34,25 +37,85 @@ function toggleControls () {
     generate.style.display = gameConfig.isStarted ? "grid" : "none";
 }
 
-startGame();
+
 function startGame () {
-    console.log(gameConfig);
     toggleControls();
     initCards();
+    rotateAllCards();
 }
 
 function  initCards() {
     const randomCardArray = getShuffledWithDuplicates(cardsArrayName, gameConfig.amount);
+    generate.style.gridTemplateColumns  =  `repeat(${Math.floor(gameConfig.amount / 2) }, 1fr)`; 
     randomCardArray.forEach(card => {
         generate.innerHTML += `
         
         <div class="box">
-
-        <img class="card-image" src="./assets/image/${card}">
+        <img class="card-image back-rotate" src='./assets/image/${card}'  />
         </div>
         `;
-        
+    });
+    document.querySelectorAll(".box").forEach(element => {
+        togleBoxClasses(element);
+        element.addEventListener("click", function() {
+            if(!this.classList.contains("rotate") && gameConfig.selectedCards.length < 2) {
+                togleBoxClasses(this);
+                gameConfig.selectedCards.push(this);
+                checkSelectedElemets();
+            }
+
+        });
+     
 
     });
+  
 
+}
+
+function rotateAllCards () {
+    setTimeout (() => {
+        document.querySelectorAll(".box").forEach(element => {
+            togleBoxClasses(element);
+        });
+
+    }, 2000);
+}
+
+function togleBoxClasses(element) {
+    element.classList.toggle("rotate");
+    element.classList.toggle("back-rotate")
+}
+
+function checkSelectedElemets() {
+    if(gameConfig.selectedCards.length === 2) {
+        const firstCard = getImageNameWithBox(gameConfig.selectedCards[0]);
+        const secondCard = getImageNameWithBox(gameConfig.selectedCards[1]);
+        console.log(firstCard, secondCard);
+        if(firstCard === secondCard) {
+            gameConfig.selectedCards.splice(0);
+            gameConfig.score++;
+            ScoreDisplay.innerHTML =  gameConfig.score;
+            if(gameConfig.score === gameConfig.amount){
+                Swal.fire({
+                    title: "Well done",
+                    icon: "success",
+                    text: "you won"
+                });
+
+            }
+                
+        }else {
+            setTimeout(() => {
+                gameConfig.selectedCards.forEach(element => {
+                    togleBoxClasses(element);
+                });
+                gameConfig.selectedCards.splice(0);
+            }, 1000)
+        }
+    }
+}
+
+
+function getImageNameWithBox(element) {
+    return element.childNodes[1].src.split("/").pop();
 }
